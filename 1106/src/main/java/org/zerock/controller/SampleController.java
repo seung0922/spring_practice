88063eba18158d8f5d40d7f352f7763678b62e75
seung <dltdlt1995@naver.com>
2019-11-06 23:00:51 +0900

    add comment

--- 1106/src/main/java/org/zerock/controller/SampleController.java
+++ 1106/src/main/java/org/zerock/controller/SampleController.java
@@ -32,27 +32,40 @@ public class SampleController {
 	@PostMapping("/exUpload")
 	public void exUploadPOST(ArrayList<MultipartFile> files) {
 		
+		// 파일을 하나도 올리지 않았을 때는 아무것도 하지않는다.
 		if(files.size() <= 0) {
 			return;
 		}
-		 
+		
+		// 받은 파일 배열을 반복문으로 돌린다.
 		files.forEach(file -> {
+			
 			System.out.println(file);
 			System.out.println(file.getSize());
 			System.out.println(file.getOriginalFilename());
 			
+			// 파일이 있을 때만 저장하도록 한다. ( 파일의 크기가 0보다 클 때는 파일을 올리지 않았다는 것이므로 )
 			if(file.getSize() > 0) {
+				
+				// 파일의 이름을 가져온다.
 				String fileName = file.getOriginalFilename();
+				
+				// 파일을 저장할 경로를 지정하고 해당 파일이름으로 저장하도록 파일 타입의 변수 선언해준다.
 				File target = new File("C:\\zzz", fileName);
 				
 				try {
+					// 파일을 위에서 지정한 폴더의 지정한 이름으로 저장시킨다.
 					FileCopyUtils.copy(file.getBytes(), target);
+					
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
 			}
+			
 		});
+		
+		System.out.println("=========================================");
 		
 	}
 	
