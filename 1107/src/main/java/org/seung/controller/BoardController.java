ad392cd31be175a882f2b58423f7e430ec5715bd
Practice <dltdlt1995@naver.com>
2019-11-08 18:56:47 +0900

    practice

--- 1107/src/main/java/org/seung/controller/BoardController.java
+++ 1107/src/main/java/org/seung/controller/BoardController.java
@@ -1,16 +1,23 @@
 package org.seung.controller;
 
+import org.seung.domain.BoardVO;
+import org.seung.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @RequestMapping("/board/*")
 @Log4j
 public class BoardController {
+	
+	@Setter(onMethod_ = { @Autowired } )
+	private BoardService service;
 
 	@GetMapping("/register")
 	public void registerGET() {
@@ -20,9 +27,15 @@ public class BoardController {
 	}
 	
 	@PostMapping("/register")
-	public void registerPOST() {
+	public String registerPOST(BoardVO vo) {
 		
 		log.info("register post................");
+		log.info(vo);
+		
+		
+		log.info(service.register(vo));
+		
+		return "redirect:/board/list";
 		
 	}
 }
